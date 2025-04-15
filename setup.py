49c79af1e56af9f01f604ac79215712c11b3a4b0
Guillenzo-hub <GZ22006@ues.edu.sv>
2025-04-14 23:57:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=['numpy'],
     author='Carlos Roberto Guillen Zometa GZ22006',
     author_email='GZ22006@ues.edu.sv',
-    url='https://github.com/tuusuario/linear-solver',
+    url='https://github.com/Guillenzo-hub/GZ22006UNO.git',
     description='Librería para resolver sistemas de ecuaciones lineales y no lineales utilizando metodos vistos en clases',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
